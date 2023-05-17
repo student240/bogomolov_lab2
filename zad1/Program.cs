eba3ca8933e617c5d98b8a53d1638a0ae09b3d52
student240 <128815643+student240@users.noreply.github.com>
2023-05-17 11:46:10 +0300

    Add files via upload

---
+++ zad1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace zad1lab2
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Введите степень: ");
+            int n = int.Parse(Console.ReadLine());
+            Console.Write("Введите число для возведения: ");
+            double d = Chislo(Console.ReadLine());
+            Console.WriteLine(func(n, d));
+            Console.ReadKey();
+        }
+
+        static double Chislo(string text)
+        {
+            if (text.Contains("."))
+            {
+                text = text.Replace(".", ",");
+            }
+            return double.Parse(text);
+        }
+
+        static double func(int n, double a)
+        {
+            if (n == 0)
+                return 1;
+            if (n == 1)
+                return a;
+            else
+            {
+                n--;
+                return a * func(n, a);
+            }
+        }
+    }
+}
