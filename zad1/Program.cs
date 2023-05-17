17f346f22f7d1bbdf37142464f4f63662dd2fbe3
student240 <128815643+student240@users.noreply.github.com>
2023-05-17 12:05:40 +0300

    Add files via upload

---
+++ zad1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace zad1lab2
+{
+    class Program
+    {
+        static void Main() //основная функция
+        {
+            Console.Write("Введите степень: ");
+            int n = int.Parse(Console.ReadLine());
+            Console.Write("Введите число для возведения: ");
+            double d = Chislo(Console.ReadLine());
+            Console.WriteLine(func(n, d));
+            Console.ReadKey();
+        }
+
+        static double Chislo(string text) //обработка вещественных чисел
+        {
+            if (text.Contains("."))
+            {
+                text = text.Replace(".", ",");
+            }
+            return double.Parse(text);
+        }
+
+        static double func(int n, double a) //функция возведения в степень
+        {
+            if (n == 0)
+                return 1;
+            if (n == 1)
+                return a;
+            else
+            {
+                n--;
+                return a * func(n, a);
+            }
+        }
+    }
+}
